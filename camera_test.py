6878ae993a46c43d9ea27cfd725e52cad392fa1e
ulan sila <edoardosila07@gmail.com>
2026-09-19 01:55:58 +0700

    add camera test script and video sender stub

---
+++ camera_test.py
@@ -0,0 +1,28 @@
+import sys
+
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+if not camera.isOpened():
+    print("ERROR: Camera gagal dibuka")
+    sys.exit()
+
+
+print("Camera berhasil dibuka")
+print("Push Q untuk exit")
+
+while True:
+    ret, frame = camera.read()
+
+    if not ret:
+        print("ERROR: Gagal membaca frame")
+        break
+
+    cv2.imshow("Drone camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
