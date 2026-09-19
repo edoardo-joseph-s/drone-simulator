0ba9e5ea726829b5212604b834c9b2eb8a658a06
ulan sila <edoardosila07@gmail.com>
2026-09-19 19:50:23 +0700

    add test script fot camera

---
+++ jpeg_test.py
@@ -0,0 +1,26 @@
+import sys
+
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+if not camera.isOpened():
+    print("Camera gagal dibuka")
+    sys.exit()
+
+ret, frame = camera.read()
+
+if not ret:
+    print("Gagal membaca fream")
+    camera.release()
+    sys.exit()
+
+succes, encoded = cv2.imencode(".jpg", frame)
+
+if not succes:
+    print("Gagal melakuakn JPEG encoding")
+else:
+    print("JPEG encoding berhasil")
+    print(f"Size JPEG: {len(encoded)} bytes")
+
+camera.release()
